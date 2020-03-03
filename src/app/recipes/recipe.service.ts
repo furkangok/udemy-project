8458958125886de8888cf342910a8640f9175c78
Furkan <muhammet.gok@boun.edu.tr>
2020-03-03 17:36:52 +0300

    update service functionalities

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
@@ -9,16 +11,24 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg'
+      'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg'
+      'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
